cacde4d8629ea69e777c8551ada54d635b547f41
Atif <peerzda2@gmail.com>
2025-05-15 22:30:03 +0500

    probably the last commit before my queue starts workinggg

--- src/models/queue.ts
+++ src/models/queue.ts
@@ -1,16 +1,15 @@
 // models/queue.ts
-import mongoose, { Schema, Document, Types } from "mongoose";
+import mongoose, { Schema, Document } from "mongoose";
 
 export interface IQueueEntry extends Document {
-  pickupPersonId: Types.ObjectId;
+  pickupPersonId: number;  // <-- Change to number
   queueNumber: number;
 }
 
 const QueueEntrySchema: Schema = new Schema({
-  pickupPersonId: { type: Schema.Types.ObjectId, required: true, ref: "appUser" },
+  pickupPersonId: { type: Number, required: true, ref: "PickupPerson" }, // <-- Number type and correct ref
   queueNumber: { type: Number, required: true },
 });
 
 export const QueueEntry = mongoose.model<IQueueEntry>("QueueEntry", QueueEntrySchema);
 export default QueueEntry;
-``
